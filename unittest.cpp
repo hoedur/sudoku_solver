f3e2997423578326d82f3075595144f5db599b25
Stefan Kratochwil <Kratochwil-LA@gmx.de>
2023-10-18 22:34:51 +0200

    Bug: Unittests for Board::index_to_block() are failing.

--- unittest.cpp
+++ unittest.cpp
@@ -5,6 +5,18 @@
 #include "board.hpp"
 
 TEST_CASE( "Accessing the contents of a solved board" ) {
+    /*
+     *  00 01 02 03 04 05 06 07 08
+     *  09 10 11 12 13 14 15 16 17
+     *  18 19 20 21 22 23 24 25 26
+     *  27 28 29 30 31 32 33 34 35
+     *  36 37 38 39 40 41 42 43 44
+     *  45 46 47 48 49 50 51 52 53
+     *  54 55 56 57 58 59 60 61 62
+     *  63 64 65 66 67 68 69 70 71
+     *  72 73 74 75 76 77 78 79 80
+     */
+
     std::vector<unsigned int> content = {
         5, 3, 4, 6, 7, 8, 9, 1, 2,
         6, 7, 2, 1, 9, 5, 3, 4, 8,
@@ -38,5 +50,158 @@ TEST_CASE( "Accessing the contents of a solved board" ) {
             auto block = b.col_by_col_index(i);
             REQUIRE(block == all_numbers);
         }
+    }
+
+    SECTION("Boundary test: Resolve index to row 0") {
+        REQUIRE(b.index_to_row(0) == 0);
+        REQUIRE(b.index_to_row(8) == 0);
+    }
+
+    SECTION("Boundary test: Resolve index to row 1") {
+        REQUIRE(b.index_to_row(9) == 1);
+        REQUIRE(b.index_to_row(17) == 1);
+    }
+
+    SECTION("Boundary test: Resolve index to row 2") {
+        REQUIRE(b.index_to_row(18) == 2);
+        REQUIRE(b.index_to_row(26) == 2);
+    }
+
+    SECTION("Boundary test: Resolve index to row 3") {
+        REQUIRE(b.index_to_row(27) == 3);
+        REQUIRE(b.index_to_row(35) == 3);
+    }
+
+    SECTION("Boundary test: Resolve index to row 4") {
+        REQUIRE(b.index_to_row(36) == 4);
+        REQUIRE(b.index_to_row(44) == 4);
+    }
+
+    SECTION("Boundary test: Resolve index to row 5") {
+        REQUIRE(b.index_to_row(45) == 5);
+        REQUIRE(b.index_to_row(53) == 5);
+    }
+
+    SECTION("Boundary test: Resolve index to row 6") {
+        REQUIRE(b.index_to_row(54) == 6);
+        REQUIRE(b.index_to_row(62) == 6);
+    }
+
+    SECTION("Boundary test: Resolve index to row 7") {
+        REQUIRE(b.index_to_row(63) == 7);
+        REQUIRE(b.index_to_row(71) == 7);
+    }
+
+    SECTION("Boundary test: Resolve index to row 8") {
+        REQUIRE(b.index_to_row(72) == 8);
+        REQUIRE(b.index_to_row(80) == 8);
+    }
+
+    SECTION("Boundary test: Resolve index to col 0") {
+        REQUIRE(b.index_to_col(0) == 0);
+        REQUIRE(b.index_to_col(72) == 0);
+    }
+
+    SECTION("Boundary test: Resolve index to col 1") {
+        REQUIRE(b.index_to_col(1) == 1);
+        REQUIRE(b.index_to_col(73) == 1);
+    }
+
+    SECTION("Boundary test: Resolve index to col 2") {
+        REQUIRE(b.index_to_col(2) == 2);
+        REQUIRE(b.index_to_col(74) == 2);
+    }
+
+    SECTION("Boundary test: Resolve index to col 3") {
+        REQUIRE(b.index_to_col(3) == 3);
+        REQUIRE(b.index_to_col(75) == 3);
+    }
+
+    SECTION("Boundary test: Resolve index to col 4") {
+        REQUIRE(b.index_to_col(4) == 4);
+        REQUIRE(b.index_to_col(76) == 4);
+    }
+
+    SECTION("Boundary test: Resolve index to col 5") {
+        REQUIRE(b.index_to_col(5) == 5);
+        REQUIRE(b.index_to_col(77) == 5);
+    }
+
+    SECTION("Boundary test: Resolve index to col 6") {
+        REQUIRE(b.index_to_col(6) == 6);
+        REQUIRE(b.index_to_col(78) == 6);
+    }
+
+    SECTION("Boundary test: Resolve index to col 7") {
+        REQUIRE(b.index_to_col(7) == 7);
+        REQUIRE(b.index_to_col(79) == 7);
+    }
+
+    SECTION("Boundary test: Resolve index to col 8") {
+        REQUIRE(b.index_to_col(8) == 8);
+        REQUIRE(b.index_to_col(80) == 8);
+    }
+
+    SECTION("Resolve index to block 0") {
+        REQUIRE(b.index_to_block(0) == 0);
+        REQUIRE(b.index_to_block(2) == 0);
+        REQUIRE(b.index_to_block(18) == 0);
+        REQUIRE(b.index_to_block(20) == 0);
+    }
+
+    SECTION("Resolve index to block 1") {
+        REQUIRE(b.index_to_block(3) == 1);
+        REQUIRE(b.index_to_block(5) == 1);
+        REQUIRE(b.index_to_block(21) == 1);
+        REQUIRE(b.index_to_block(23) == 1);
+    }
+
+    SECTION("Resolve index to block 2") {
+        REQUIRE(b.index_to_block(6) == 2);
+        REQUIRE(b.index_to_block(8) == 2);
+        REQUIRE(b.index_to_block(24) == 2);
+        REQUIRE(b.index_to_block(26) == 2);
+    }
+
+    SECTION("Resolve index to block 3") {
+        REQUIRE(b.index_to_block(27) == 3);
+        REQUIRE(b.index_to_block(29) == 3);
+        REQUIRE(b.index_to_block(45) == 3);
+        REQUIRE(b.index_to_block(47) == 3);
+    }
+
+    SECTION("Resolve index to block 4") {
+        REQUIRE(b.index_to_block(30) == 4);
+        REQUIRE(b.index_to_block(32) == 4);
+        REQUIRE(b.index_to_block(48) == 4);
+        REQUIRE(b.index_to_block(50) == 4);
+    }
+
+    SECTION("Resolve index to block 5") {
+        REQUIRE(b.index_to_block(33) == 5);
+        REQUIRE(b.index_to_block(35) == 5);
+        REQUIRE(b.index_to_block(51) == 5);
+        REQUIRE(b.index_to_block(53) == 5);
+    }
+
+    SECTION("Resolve index to block 6") {
+        REQUIRE(b.index_to_block(54) == 6);
+        REQUIRE(b.index_to_block(56) == 6);
+        REQUIRE(b.index_to_block(72) == 6);
+        REQUIRE(b.index_to_block(74) == 6);
+    }
+
+    SECTION("Resolve index to block 7") {
+        REQUIRE(b.index_to_block(57) == 7);
+        REQUIRE(b.index_to_block(59) == 7);
+        REQUIRE(b.index_to_block(75) == 7);
+        REQUIRE(b.index_to_block(77) == 7);
+    }
+
+    SECTION("Resolve index to block 8") {
+        REQUIRE(b.index_to_block(60) == 8);
+        REQUIRE(b.index_to_block(62) == 8);
+        REQUIRE(b.index_to_block(78) == 8);
+        REQUIRE(b.index_to_block(80) == 8);
     }
 }
